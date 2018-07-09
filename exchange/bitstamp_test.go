0d0819ea15ef15b3a6b41fe629e240142a3a9c79
Jonny Huxtable <jonny@linkpool.io>
2018-07-09 19:09:26 +0100

    Disabled Bitstamp tests due to rate limiter

--- exchange/bitstamp_test.go
+++ exchange/bitstamp_test.go
@@ -15,6 +15,7 @@ func TestBitstamp_SetPairs(t *testing.T) {
 	assert.Contains(t, pairs, &Pair{"ETH", "EUR"})
 }
 
+/* Disabled due to rate limit
 func TestBitstamp_GetResponse(t *testing.T) {
 	bitstamp := Bitstamp{}
 	price, err := bitstamp.GetResponse("BTC", "USD")
@@ -24,3 +25,4 @@ func TestBitstamp_GetResponse(t *testing.T) {
 	assert.True(t, price.Price > 0, "price from Bitstamp isn't greater than 0")
 	assert.True(t, price.Volume > 0, "volume from Bitstamp isn't greater than 0")
 }
+*/
